69d10f1e5b025a9e0b31344db940ee9ad12a0c74
Semin Park <SPark9625@gmail.com>
2020-07-29 15:50:05 +0900

    Change API URL

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL;
 
 export async function listVisitLogs(userId) {
     const response = await axios.get(`${API_URL}/api/logs/all/${userId}`);
